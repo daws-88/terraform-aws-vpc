3721bf3da892f09d7a3cc632c471dcdb0e5aad93
Venkat <psai96526@gmail.com>
2026-05-13 15:50:08 +0530

    now

--- vpc.tf
+++ vpc.tf
@@ -85,7 +85,7 @@ resource "aws_subnet" "database" {
   )
 }
 
-# private rout table
+# private route table
  resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
   tags = merge(
@@ -97,7 +97,7 @@ resource "aws_subnet" "database" {
   )
 }
 
-## rout table for database
+## route table for database
  resource "aws_route_table" "database" {
   vpc_id = aws_vpc.main.id
   tags = merge(
@@ -128,7 +128,7 @@ resource "aws_eip" "nat" {
   )
 }
 
-# nat 
+# NAT
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public[0].id
@@ -144,10 +144,37 @@ resource "aws_nat_gateway" "nat" {
   # on the Internet Gateway for the VPC.
   depends_on = [aws_internet_gateway.main]
 }
- ## create private route 
+ ### Private egress route through NAT
  resource "aws_route" "private" {
   route_table_id = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.nat.id
 }
 
+## Database egress route through NAT
+resource "aws_route" "database" {
+  route_table_id = aws_route_table.database.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.nat.id
+}
+
+### piblic subnet associations ###
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidrs)
+  subnet_id = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+#### private  subnet associations ###
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidrs)
+  subnet_id = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+#### database subnet associations ###
+resource "aws_route_table_association" "database" {
+  count = length(var.database_subnet_cidrs)
+  subnet_id = aws_subnet.database[count.index].id
+  route_table_id = aws_route_table.database.id
+}
