fec7745cb463130f6ce9e24a569a625673d3f33e
Venkat <psai96526@gmail.com>
2026-05-13 10:14:25 +0530

    now

---
+++ variable.tf
@@ -0,0 +1,76 @@
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "project_name" {
+  type =  string
+}
+
+variable "environment" {
+  type =  string
+}
+
+variable "vpc_tags" {
+  type = map
+  default = {
+  }
+}
+variable "igw_tags" {
+  type = map
+  default = {
+  }
+}
+
+variable "public_subnet_cidrs" {
+    type = list
+}
+
+variable "public_subnet_tags" {
+  type = map
+  default = {}
+}
+
+variable "private_subnet_cidrs" {
+    type = list
+}
+
+variable "private_subnet_tags" {
+    type = map
+    default = {}
+}
+
+variable "database_subnet_tags" {
+    type = map
+    default = {}
+}
+
+variable "database_subnet_cidrs" {
+    type = list
+}
+
+variable "public_route_table_tags" {
+    type = map
+    default = {}
+}
+
+variable "private_route_table_tags" {
+     type = map
+     default = {}
+}
+  
+variable "database_route_table_tags" {
+     type = map
+    default = {}
+}
+  
+variable "elastic_ip_tags" {
+  type = map
+  default = {}
+}
+
+variable "nat_gateway_tags" {
+   type = map
+  default = {}
+}
+
+
