dd7d695f5b593c86ec68113b76d40c1ac40500e8
Venkat <psai96526@gmail.com>
2026-05-14 15:28:32 +0530

    up

--- data.tf
+++ data.tf
@@ -1,4 +1,19 @@
 data "aws_availability_zones" "available" {
   state = "available"
 }
- 
+
+output "az_names" {
+  value = data.aws_availability_zones.available.names
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_route_table" "default" {
+  vpc_id = data.aws_vpc.default.id
+  filter {
+    name   = "association.main"
+    values = ["true"]
+  }
+}
